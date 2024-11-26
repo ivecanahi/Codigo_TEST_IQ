d933e2c761fe0cc673531e970a739216251e5cdd
Miguel-unl <miguel.armas@unl.edu.ec>
2024-11-25 20:25:46 -0500

    Metodos creados en las diferentes clases y comentarios a rasgos generales de que hacer, en Test.java solo realizar lo que los comentarios reaizados que lo demás requiere areglos y la clase GestorPreguntas

--- src/views/MainTest.java
+++ src/views/MainTest.java
@@ -1,5 +1,15 @@
 package Codigo_TEST_IQ.src.views;
 
+import controller.resources.Test;
 public class MainTest {
-
+    public static void main(String[] args){
+        //Preguntas de prueba para comprobar el funcionamiento
+        String[] preguntas = {"Hola como estas?@si@mal@Ðdimelo@no",
+            "Cual de los siguientes anagramas es equivalente a ADAN?@Nadan@Andan@ÐNada@Anden",
+            "Quien da programación?@Luis@Miguel@Mi vecino@ÐJose Guaman",
+            "De que tienes hambre?@pollo@repe@bolon@Ðpastel"};
+        
+            //imprime solo los aciertos para comprobar el funcionamiento
+        System.out.println(Test.tomarTest("Direccion", 2, 1, 4, 4, preguntas));
+    }
 }
